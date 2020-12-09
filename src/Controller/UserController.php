beae2b1160660c4af2763c582ef976347f41d681
Damien008 <damien.lambert999@gmail.com>
2020-12-09 10:33:27 +0100

    fix src/Controller/UserController.php add route

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,10 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/users", name="user_")
+ */
+
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="user")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
