55fb3b6a72e9c35b741e85172e508a5ed86657f3
AxelRaboit <axel.raboit@gmail.com>
2020-12-16 13:16:41 +0100

    SearchController added

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\SearchUserType;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+ /**
+* @Route("/search", name="search_")
+*/
+class SearchController extends AbstractController
+{
+    /**
+     * @Route("/", name="level")
+     */
+    public function search(Request $request, UserRepository $userRepository): Response
+    {
+        $users = new User();
+        $searchUserForm = $this->createForm(SearchUserType::class, $users);
+        $searchUserForm->handleRequest($request);
+
+        if ($searchUserForm->isSubmitted() && $searchUserForm->isValid()) {
+            $level = $users->getLevel();
+            $sex = $users->getSex();
+            $users = $userRepository->search($level, $sex);
+        } else {
+            $users = $userRepository->findAll();
+        }
+        return $this->render('search/index.html.twig', [
+            'searchForm' => $searchUserForm->createView(),
+            'users' => $users,
+        ]);
+    }
+}
