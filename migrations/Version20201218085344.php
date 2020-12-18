01d3b145b7a58d6eed84536537cad2d203d23a99
alexandre corrette <alexandrecorrette@gmail.com>
2020-12-18 10:09:05 +0100

    conflicts resolved

---
+++ migrations/Version20201218085344.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201218085344 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD address VARCHAR(100) DEFAULT NULL, ADD postalcode VARCHAR(50) DEFAULT NULL, ADD city VARCHAR(100) NOT NULL, ADD description VARCHAR(255) NOT NULL, ADD email VARCHAR(100) NOT NULL, ADD phone VARCHAR(50) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP address, DROP postalcode, DROP city, DROP description, DROP email, DROP phone');
+    }
+}
