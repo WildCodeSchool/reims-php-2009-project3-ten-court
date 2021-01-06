f3547dd5e723e07cd918489bf2339c10ff805d7c
AxelRaboit <axel.raboit@gmail.com>
2021-01-06 13:57:38 +0100

    login and auth completed

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,11 +5,13 @@ namespace App\Entity;
 use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\UserRepository;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints\Date;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class User implements UserInterface
 {
@@ -271,6 +273,7 @@ class User implements UserInterface
 
         return $this;
     }
+//We have to work on it
 
 /*     public function getAge(): int
     {
