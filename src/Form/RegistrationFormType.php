f7f3df2d08a82bba648355f556306e2b5648ef54
AxelRaboit <axel.raboit@gmail.com>
2021-01-06 17:21:13 +0100

    registration form completed with all inputs

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,50 +4,94 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+        ->add('email', TextType::class, [
+            'label' => 'Email',
+            'attr' => [
+                'placeholder' => 'Ex: prenom-nom@gmail.com'
+            ]
+        ])
             ->add('agreeTerms', CheckboxType::class, [
+                'label' => 'Accepter les termes et conditions',
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Vous devez accepter les termes et conditions',
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+            ->add('plainPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options'  => array('label' => 'Mot de passe'),
+                'second_options' => array('label' => 'Confirmer le mot de passe'),
+                'invalid_message' => 'Le mot de passe doit être identique',
                 'mapped' => false,
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez entrer votre mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit faire au moins {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
+            ))
+            ->add('pseudo', TextType::class, [
+                'label' => 'Pseudo',
+                'attr' => [
+                    'placeholder' => 'Ex: Albert51'
+                ]
             ])
-            ->add('pseudo')
-            ->add('sex')
-            ->add('level')
-            ->add('city')
-            ->add('birthdate')
+            ->add('sex', ChoiceType::class, [
+                'label' => 'Sexe',
+                'choices'  => [
+                    'Homme' => 'homme',
+                    'Femme' => 'femme',
+                    'Autre' => 'autre',
+                ]
+            ])
+            ->add('level', ChoiceType::class, [
+                'label' => 'Niveau',
+                'choices'  => [
+                    'Débutant' => 'debutant',
+                    'Intermediaire' => 'intermediaire',
+                    'Expert' => 'expert',
+                    'Professionnel' => 'professionnel',
+                ]
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville',
+                'attr' => [
+                    'placeholder' => 'Ex: Toulouse'
+                ]
+            ])
+            ->add('birthdate', BirthdayType::class, [
+                'label' => 'Date de naissance',
+                'format' => 'dd-MM-yyyy'
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Soumettre',
+            ]);
 
         ;
     }
