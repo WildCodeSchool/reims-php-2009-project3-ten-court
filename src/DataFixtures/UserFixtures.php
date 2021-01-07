ee0c55a07697ad44710aa0d602916695ede926a7
AxelRaboit <axel.raboit@gmail.com>
2021-01-07 01:11:53 +0100

    modification in fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -21,17 +21,69 @@ class UserFixtures extends Fixture
     {
         // Création d’un utilisateur de type “contributeur” (= auteur)
         $contributor = new User();
-        $contributor->setEmail('contributor@monsite.com');
+        $contributor->setEmail('axel@gmail.com');
         $contributor->setRoles(['ROLE_CONTRIBUTOR']);
-        $contributor->setPseudo('Contributor');
+        $contributor->setPseudo('Axel');
         $contributor->setSex('Homme');
-        $contributor->setLevel('Expert');
-        $contributor->setBirthdate(new DateTime('1955/02/24'));
-        $contributor->setCity('California');
-        $contributor->setDescription('Contributor Account');
+        $contributor->setLevel('intermediaire');
+        $contributor->setPhone('438-396-1459');
+        $contributor->setBirthdate(new DateTime('1990/11/30'));
+        $contributor->setCity('Montréal');
+        $contributor->setDescription('Je souhaite commencer à jouer au tennis');
         $contributor->setPassword($this->passwordEncoder->encodePassword(
             $contributor,
-            'contributorpassword'
+            'password'
+        ));
+        $manager->persist($contributor);
+
+        // Création d’un utilisateur de type “contributeur” (= auteur)
+        $contributor = new User();
+        $contributor->setEmail('damien@gmail.com');
+        $contributor->setRoles(['ROLE_CONTRIBUTOR']);
+        $contributor->setPseudo('Damien');
+        $contributor->setSex('Homme');
+        $contributor->setLevel('expert');
+        $contributor->setPhone('06 30 20 10 90');
+        $contributor->setBirthdate(new DateTime('1976/02/24'));
+        $contributor->setCity('Rethel');
+        $contributor->setDescription('Je voudrais rencontrer du monde afin de jouer et être plus experimenté');
+        $contributor->setPassword($this->passwordEncoder->encodePassword(
+            $contributor,
+            'password'
+        ));
+        $manager->persist($contributor);
+
+        // Création d’un utilisateur de type “contributeur” (= auteur)
+        $contributor = new User();
+        $contributor->setEmail('alexandre@gmail.com');
+        $contributor->setRoles(['ROLE_CONTRIBUTOR']);
+        $contributor->setPseudo('Alexandre');
+        $contributor->setSex('Homme');
+        $contributor->setLevel('débutant');
+        $contributor->setPhone('202-555-0101');
+        $contributor->setBirthdate(new DateTime('1970/04/10'));
+        $contributor->setCity('California');
+        $contributor->setDescription('J\'ai décidé de changer de sport et de me mettre au tennis');
+        $contributor->setPassword($this->passwordEncoder->encodePassword(
+            $contributor,
+            'password'
+        ));
+        $manager->persist($contributor);
+
+        // Création d’un utilisateur de type “contributeur” (= auteur)
+        $contributor = new User();
+        $contributor->setEmail('olivier@gmail.com');
+        $contributor->setRoles(['ROLE_CONTRIBUTOR']);
+        $contributor->setPseudo('Olivier');
+        $contributor->setSex('Homme');
+        $contributor->setLevel('professionnel');
+        $contributor->setPhone('06 21 29 98 10');
+        $contributor->setBirthdate(new DateTime('1970/07/03'));
+        $contributor->setCity('Troyes');
+        $contributor->setDescription('Je souhaite rencontrer des personnes pour jouer');
+        $contributor->setPassword($this->passwordEncoder->encodePassword(
+            $contributor,
+            'password'
         ));
         $manager->persist($contributor);
 
@@ -42,8 +94,9 @@ class UserFixtures extends Fixture
         $admin->setPseudo('Administrator');
         $admin->setSex('Homme');
         $admin->setLevel('Expert');
+        $admin->setPhone('06 98 98 98 99');
         $admin->setBirthdate(new DateTime('2000/02/24'));
-        $admin->setCity('California');
+        $admin->setCity('Nowhere');
         $admin->setDescription('Admin Account');
         $admin->setPassword($this->passwordEncoder->encodePassword(
             $admin,
