a9cb5400fb31c34e63c1f5fb7e6edc0a2a1f0446
Damien008 <damien.lambert999@gmail.com>
2021-01-12 16:29:26 +0100

    add function generate

--- src/Service/Slugify.php
+++ src/Service/Slugify.php
@@ -10,15 +10,16 @@ class Slugify
      */
     public function generate(string $slug): string
     {
-        $slug = preg_replace('~[^\pL\d]+~u', '-', $slug);
-        $slug = iconv('utf-8', 'us-ascii//TRANSLIT', $slug);
-        $slug = preg_replace('~[^-\w]+~', '', $slug);
-        $slug = trim($slug, '-');
-        $slug = preg_replace('~-+~', '-', $slug);
-        $slug = strtolower($slug);
+
+          $slug = preg_replace('~[^\pL\d]+~u', '-', $slug);
+          $slug = iconv('utf-8', 'us-ascii//TRANSLIT', $slug ?? '');
+          $slug = preg_replace('~[^-\w]+~', '', $slug);
+          $slug = trim($slug, '-');
+          $slug = preg_replace('~-+~', '-', $slug);
+          $slug = strtolower($slug ?? '');
 
         if (empty($slug)) {
-             return 'n-a';
+            return 'n-a';
         }
 
         return $slug;
