9b36a383ef3fcd3334af907a9043ddf7fb936824
Damien008 <damien.lambert999@gmail.com>
2021-01-12 15:44:25 +0100

    fix add slug

--- migrations/Version20210112091429.php
+++ migrations/Version20210112091429.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201218091526 extends AbstractMigration
+final class Version20210112091429 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,12 +20,12 @@ final class Version20201218091526 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD address VARCHAR(100) DEFAULT NULL, ADD postalcode VARCHAR(50) DEFAULT NULL, ADD city VARCHAR(100) NOT NULL, ADD description VARCHAR(255) NOT NULL, ADD email VARCHAR(100) NOT NULL, ADD phone VARCHAR(50) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD avatar VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP address, DROP postalcode, DROP city, DROP description, DROP email, DROP phone');
+        $this->addSql('ALTER TABLE user DROP avatar');
     }
 }
