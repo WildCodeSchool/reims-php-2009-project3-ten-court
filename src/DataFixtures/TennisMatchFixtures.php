09f29fb5e9d85ce51cbbfae0f92c6a3d77313d20
Damien008 <damien.lambert999@gmail.com>
2021-01-19 15:20:13 +0100

    feat match fixtures

---
+++ src/DataFixtures/TennisMatchFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\TennisMatch;
+use App\DataFixtures\UserFixtures;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
+class TennisMatchFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function getDependencies()
+    {
+        return [UserFixtures::class];
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+
+        //Generate Women Users
+        for ($i = 0; $i < 50; $i++) {
+            // Création d’un utilisateur de type “contributeur” (= auteur)
+            $match = new TennisMatch();
+            $match->setStartHour($faker->dateTimeBetween('now', '2 years'));
+            $match->setEndHour($faker->dateTimeBetween('now', '2 years'));
+            $match->setName('match amical');
+            $match->setDescription($faker->paragraph());
+            $match->setAdress($faker->city);
+            $match->setOrganizer($this->getReference('admin'));
+            $manager->persist($match);
+        }
+        $manager->flush();
+    }
+}
