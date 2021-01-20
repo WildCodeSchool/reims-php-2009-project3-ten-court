a323b8cecd2acba9a2ffa3bfe6026458e195ca4c
Damien008 <damien.lambert999@gmail.com>
2021-01-20 12:03:03 +0100

    new migration for change of entity TennisMatch

---
+++ migrations/Version20210120092948.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210120092948 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE tennis_match ADD event_date DATE NOT NULL, CHANGE start_hour start_hour TIME NOT NULL, CHANGE end_hour end_hour TIME NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE tennis_match DROP event_date, CHANGE start_hour start_hour DATETIME NOT NULL, CHANGE end_hour end_hour DATETIME NOT NULL');
+    }
+}
