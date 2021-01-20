185719717ec20d03c9c5e9cce67a3d3640ebfa6a
Alexandre Corrette <alexandrecorrette@MacBook-Air-de-Alexandre.local>
2021-01-20 11:42:07 +0100

    fix : add a unique entity on pseudo

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -15,6 +15,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"pseudo"}, message="There is already an account with this pseudo")
  */
 class User implements UserInterface
 {
