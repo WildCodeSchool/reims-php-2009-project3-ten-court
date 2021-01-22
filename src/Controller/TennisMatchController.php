098c50b629b5eeea4f34c831563fff242b397b2b
AxelRaboit <axel.raboit@gmail.com>
2021-01-22 09:39:07 +0100

    path change in the tennismatchcontroller the redirection

--- src/Controller/TennisMatchController.php
+++ src/Controller/TennisMatchController.php
@@ -44,7 +44,7 @@ class TennisMatchController extends AbstractController
             $entityManager->persist($tennisMatch);
             $entityManager->flush();
 
-            return $this->redirectToRoute('search_matchs');
+            return $this->redirectToRoute('search_matches');
         }
 
         return $this->render('tennis_match/new.html.twig', [
