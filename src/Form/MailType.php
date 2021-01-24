d5289e54131a23edaac3ef90e513db0b4a540430
olivier-preaux <olivier.prx@gmail.com>
2021-01-25 00:51:25 +0100

    feat : New function Email

---
+++ src/Form/MailType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Email;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+class MailType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('from', EmailType::class)    
+            ->add('to', EmailType::class ,array('attr' => array('readonly' => true)))    
+            ->add('subject', TextType::class)
+            ->add('html', TextareaType::class)
+            
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+               
+        ]);
+    }
+}
