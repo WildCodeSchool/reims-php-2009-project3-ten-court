88a996e1c33973f63855247c2292682ba4916d3c
Damien008 <damien.lambert999@gmail.com>
2021-01-26 14:05:20 +0100

    feat form to change password

---
+++ src/Form/UpdatePasswordType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class UpdatePasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('oldPassword', PasswordType::class, [
+            'label' => 'Ancien mot de passe',
+            'mapped' => false
+        ])
+        ->add('plainPassword', RepeatedType::class, array(
+            'type' => PasswordType::class,
+            'first_options'  => array(
+                'label' => 'Nouveau mot de passe',
+            ),
+            'second_options' => array(
+                'label' => 'Confirmer le mot de passe',
+            ),
+            'invalid_message' => 'Le mot de passe doit être identique',
+            'mapped' => false,
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'Veuillez entrer votre mot de passe',
+                ]),
+                new Length([
+                    'min' => 6,
+                    'minMessage' => 'Votre mot de passe doit faire au moins {{ limit }} caractères',
+                    // max length allowed by Symfony for security reasons
+                    'max' => 4096,
+                ]),
+            ],
+        ))
+        ->add('submit', SubmitType::class, [
+            'label' => "Mettre à jour",
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
