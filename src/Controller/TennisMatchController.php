ac7bf2317a9654d91c8be6530b33d54acc19d6a4
olivier-preaux <olivier.prx@gmail.com>
2021-01-26 10:53:32 +0100

    feat: new button see organizer's profil in match template

--- src/Controller/TennisMatchController.php
+++ src/Controller/TennisMatchController.php
@@ -58,7 +58,7 @@ class TennisMatchController extends AbstractController
     /**
      * @Route("/{id}", name="tennis_match_show", methods={"GET"})
      */
-    public function show(TennisMatch $tennisMatch): Response
+    public function show(TennisMatch $tennisMatch , User $user): Response
     {
         $participents = $tennisMatch->getParticipent();
         $nbParticipents = count($participents);
@@ -72,6 +72,7 @@ class TennisMatchController extends AbstractController
             'tennis_match' => $tennisMatch,
             'nbParticipents' => $nbParticipents,
             'isParticipent' => $isParticipent,
+            'user' => $user
         ]);
     }
 
